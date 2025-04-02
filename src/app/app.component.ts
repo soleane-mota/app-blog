5affefdd32f9019f71c7e93c2f478ef775fc8c99
Soleane Mota <sem.sol@gmail.com>
2025-04-02 17:54:33 -0400

    feat: create menu title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
