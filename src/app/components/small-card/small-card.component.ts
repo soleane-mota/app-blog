eee2c9e26498d3766a2fc5c90100720b1b9e20d7
Soleane Mota <sem.sol@gmail.com>
2025-04-03 08:04:03 -0400

    feat: decouple the small card component

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+  @Input()
+  imageCover: string = '';
+  @Input()
+  imageSource: string = '';
+  @Input()
+  imageAlt: string = '';
+  @Input()
+  cardTitle: string = '';
 }
